57c25098fc223c7bc7a0cee52d786227f42b89c1
Taehyeong Kim <th.imk3@gmail.com>
2023-03-11 10:33:35 +0900

    config : use ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,19 @@ import Header from "@components/header";
 import "@styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import ReactGA from "react-ga";
+
+ReactGA.initialize(process.env.NEXT_PUBLIC_GA_TRACKING_ID as string);
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, [router.asPath]);
+
   return (
     <SessionProvider session={session}>
       <Header />
