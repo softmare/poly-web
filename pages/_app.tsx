869d8439da3dbb07417c37b5e492eae7b8f1c700
Taehyeong Kim <th.imk3@gmail.com>
2023-03-26 21:55:59 +0900

    impl GA second way

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,31 @@ import Header from "@components/header";
 import "@styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  const gid = process.env.NEXT_PUBLIC_GA_TRACKING_ID as string;
   return (
-    <SessionProvider session={session}>
-      <Header />
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gid}`}
+      />
+      <Script id="google-analytics" strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${gid}', {
+          page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+      <SessionProvider session={session}>
+        <Header />
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   );
 }
 
